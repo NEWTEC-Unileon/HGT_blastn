1e04f051a01f27ae6d5d2bca269b09acac8b5f1d
NEWTEC-Unileon <jcobd@unileon.es>
2026-04-28 12:57:34 +0200

    Update 01.change_genomes_headers.rb

--- 01.change_genomes_headers.rb
+++ 01.change_genomes_headers.rb
@@ -2,16 +2,17 @@
 
 name=''
 out=File.new("all_genomes.fna","w")
-
-aa=File.open("list_genomes.txt").each_line do |file|
+`ls #{ARGV[0]} | sed "s/.fna//g" | sed "s/.fa//g" | sed "s/.fasta//g" > list.txt`
+aa=File.open("list.txt").each_line do |file|
 file.chomp!
-name=file.gsub("bins","mag")
+#name=file.gsub("bins","mag")
+name=file
 puts name
-	bb=File.open("../MAGs_613/#{file}").each_line do |line|
+	bb=File.open("#{ARGV[0]}/#{file}"*).each_line do |line|
 	line.chomp!
 #>contig00001 len=345504 cov=28.1 corr=0 origname=NODE_1_length_345504_cov_28.082128_pilon sw=shovill-spades/1.1.0 date=20210115
-	if line =~ /^>.*(NODE\S+length\_\d+\_\S+)/
-		out.puts ">#{name}_#{$1}"
+	if line =~ /^>/
+		out.puts ">#{name}_#{line}"
 	else
 		out.puts line
 	end
