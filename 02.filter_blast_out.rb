76a71e47e8686f036b9d1d58f40d9729b63d367a
NEWTEC-Unileon <jcobd@unileon.es>
2026-04-28 13:01:47 +0200

    Update 02.filter_blast_out.rb

--- 02.filter_blast_out.rb
+++ 02.filter_blast_out.rb
@@ -11,9 +11,9 @@ out=File.new("blastn_999_filtered.txt","w")
 
 aa=File.open("blastn_mags.txt").each_line do |line|
 line.chomp!
-#Ecoli_153_NODE_3_length_299180_cov_19.295838	Ecoli_214_NODE_6_length_181195_cov_28.064184	100.000	41	0	0	152369	152409	55123	55083	3.30e-09	76.8
-if line =~ /^(\S+)\_NODE\S+\s+(\S+)\_NODE\S+\s+/
-	if $1!=$2 and line.split("\t")[3].to_i>500
+#Ecoli153_NODE_3_length_299180_cov_19.295838	Ecoli214_NODE_6_length_181195_cov_28.064184	100.000	41	0	0	152369	152409	55123	55083	3.30e-09	76.8
+if line =~ /^(\S+)\_\_\_\S+\s+(\S+)\_\_\_\S+\s+/
+	if $1!=$2 and line.split("\t")[3].to_i>500 and line.split("\t")[2].to_f>99.9.to_f
 	out.puts line
 	end
 end
